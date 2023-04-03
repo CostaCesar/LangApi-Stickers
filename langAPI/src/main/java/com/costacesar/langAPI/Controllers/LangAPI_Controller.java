467ee8c5395412b4b0be0c5666bad5d4c8ef86ef
CostaCesar <caioplays@outlook.com>
2023-04-03 17:21:28 -0300

    ADD: Elementos CRUD

--- langAPI/src/main/java/com/costacesar/langAPI/Controllers/LangAPI_Controller.java
+++ langAPI/src/main/java/com/costacesar/langAPI/Controllers/LangAPI_Controller.java
@@ -1,14 +1,10 @@
 package com.costacesar.langAPI.Controllers;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.costacesar.langAPI.Models.Language;
 import com.costacesar.langAPI.Repos.LangRepo;
@@ -18,18 +14,8 @@ public class LangAPI_Controller
 {
     @Autowired
     private LangRepo repository;
-    // private List<Language> langList =
-    //     List.of(
-    //         new Language("Java", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/java/java_512x512.png", 4),
-    //         new Language("C++", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/cpp/cpp_512x512.png", 6),
-    //         new Language("C", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/c/c_512x512.png", 1),
-    //         new Language("C#", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/csharp/csharp_512x512.png", 2),
-    //         new Language("HTML", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/html/html_512x512.png", 7),
-    //         new Language("CSS", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/css/css_512x512.png", 5),
-    //         new Language("Python", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/python/python_512x512.png", 3)
-    //     );
 
-    @GetMapping(value="hello")
+    @GetMapping(value="/hello")
     public String getTest()
     {
         return "Hello, Spring!";
@@ -43,13 +29,31 @@ public class LangAPI_Controller
         return output;
     }
     
-    // TODO: Get by id
-    // @GetMapping(value="/LangList/{avlue}")
-    // public Language getLang(@PathVariable("title") String lang)
-    // {
-    //     Language output = repository.findById(lang);
-    //     return output;
-    // }
+    @GetMapping(value="/LangList/{title}")
+    public Language getLang(@PathVariable String title)
+    {
+        Optional<Language> output = repository.findBytitle(title);
+        return output.get();
+    }
+
+    @PutMapping(value="/LangList")
+    public List<Language> updateLangList(@RequestBody Language updated)
+    {   
+        repository.save(updated);
+        List<Language> output = repository.findAll();
+        Collections.sort(output, (x, y) -> x.getRanking() - y.getRanking());
+        return output;
+    }
+
+
+    @DeleteMapping(value="/LangList/{title}")
+    public List<Language> deleteLangList(@PathVariable String title)
+    {   
+        repository.deleteBytitle(title);
+        List<Language> output = repository.findAll();
+        Collections.sort(output, (x, y) -> x.getRanking() - y.getRanking());
+        return output;
+    }
 
     @PostMapping(value="/LangList")
     public Language postLangList(@RequestBody Language add)
--- langAPI/src/main/java/com/costacesar/langAPI/Repos/LangRepo.java
+++ langAPI/src/main/java/com/costacesar/langAPI/Repos/LangRepo.java
@@ -1,10 +1,13 @@
 package com.costacesar.langAPI.Repos;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.costacesar.langAPI.Models.Language;
 
 public interface LangRepo extends MongoRepository<Language, String>
 {
-    
+    Optional<Language> findBytitle(String title);
+    Optional<Language> deleteBytitle(String title);
 }
